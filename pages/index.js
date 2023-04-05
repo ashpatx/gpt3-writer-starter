92ff5020c2009b37ba25cac1ac398b31d47ad3d2
ashpatx <thingslearn3@gmail.com>
2023-04-04 20:58:32 -0400

    initial setup to gpt3 writer generator
    setup for card prompt generator

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
   return (
     <div className="root">
       <Head>
@@ -11,13 +41,28 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>send a quick e-card today</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>share a lil message and make a person's day!</h2>
           </div>
         </div>
-      </div>
+        <div className="prompt-container">
+          <textarea 
+            className="prompt-box"
+            placeholder="start typing here" 
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={callGenerateEndpoint}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
+          </div>
+          </div>
+        </div>
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
